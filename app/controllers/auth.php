27e291bb34a4c44a245bafc07fd775a009359607
Dılo abinin yeri <dilsizkaval@windowslive.com>
2016-10-03 02:21:03 +0300

    Update auth.php

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -6,26 +6,36 @@
  * Date: 1.10.2016
  * Time: 20:50
  */
-include_once "system/session/login.php";
-include_once "system/session/logout.php";
 
 
 class auth
 {
 
-    use login,logout;
 
- public function authlogin(){
 
-login::login();
+ public function authlogin($userdata=false){
+
+     session_start();
+
+     if(is_array($userdata)){
+
+         foreach ($userdata as $key=>$value){
+
+
+             $_SESSION["user"][$key]=$value;
+
+         }
+
+     }
  }
 
 
 
  public function authlogout(){
 
+     session_start();
+     session_destroy();
 
-logout::logout();
 
  }
 
