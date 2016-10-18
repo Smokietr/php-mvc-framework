ccf3fa8ab3008c4dc7ef55acf63b36d34e3eea50
Dılo abinin yeri <dilsizkaval@windowslive.com>
2016-10-18 20:15:19 +0300

    Update craud.php

--- system/database/craud.php
+++ system/database/craud.php
@@ -17,21 +17,29 @@ private $result;
     private $dbname="deneme";
     private $user="root";
     private $password="";
+    private $where;
     protected $connect;
+    private $array;
+    private $col;
 
     function __construct(){
 
-        $this->connect=new PDO("mysql:host=".$this->host.";dbname=".$this->dbname.";charset=utf8",$this->user,$this->password);
+        $this->connect=new \PDO("mysql:host=".$this->host.";dbname=".$this->dbname.";charset=utf8",$this->user,$this->password);
 
     }
 
+    function __set($name, $value)
+    {
+        $this->col[$name]=$value;
+
+    }
 
     public function find($id){
 
         $this->result=$this->connect->prepare("select * from ".$this->table." where id=:id");
         $this->result->bindParam("id",$id);
         $this->result->execute();
-        $this->result=$this->result->fetch(PDO::FETCH_ASSOC);
+        $this->result=$this->result->fetch(\PDO::FETCH_ASSOC);
 
        return $this->result;
 
@@ -41,7 +49,48 @@ private $result;
 
 
 
+    public function where($where){
 
+     $this->where=" where $where";
+
+        return $this;
+    }
+
+
+
+    public function get(){
+
+        $this->result=$this->connect->query("select * from ".$this->table.$this->where);
+
+       while($a=$this->result->fetch(PDO::FETCH_ASSOC)){
+
+
+           $this->array[]=$a;
+       }
+
+
+       return $this->array;
+
+
+
+
+    }
+
+
+
+
+
+
+    public function delete(){
+
+return $this->connect->exec("delete  from ".$this->table.$this->where);
+
+
+
+
+
+
+    }
 
 
 }
