a63a702393ea05f62c087700bc5de1ca6a0d9cb3
Dılo abinin yeri <dilsizkaval@windowslive.com>
2016-12-10 00:58:36 +0200

    Update Router.php

--- system/router/Router.php
+++ system/router/Router.php
@@ -130,7 +130,7 @@ public static function post($url,$callback){
 
 
 
-       if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $url=ltrim($url,"/");
 
        
@@ -173,33 +173,50 @@ self::$dynUrl=explode("/",self::$url);
 //post end
 
 //put start
-    public static function put($url,$callback){
-
-        if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-
-            if(count(array_filter(explode("/",self::$url))==count(array_filter(explode("/",$url))))) {
-
-
-                if(explode("/",trim(self::$url,"/"))[0]==explode("/",trim($url,"/"))[0]){
-
-                    self::$dynUrl=explode("/",self::$url);
-
-
-                    Router::mainRouter($url, $callback);
-
-                }
+  public static function put($url,$callback){
 
 
 
+       if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+$url=ltrim($url,"/");
+
+       
+if(array_filter(explode("/",self::$url)) == array_filter(explode("/",$url))){
+
+ 
+
+      self::$dynUrl=explode("/",self::$url);
+
+
+         Router::mainRouter($url, $callback);
+
+   }
+      else
+      
+       {
+
+self::$explode=explode("/",$url);
+self::$joker=end(self::$explode);
+if(self::$joker == "*" && explode("/",$url)[0]==explode("/",self::$url)[0]){
+self::$dynUrl=explode("/",self::$url);
+
+
+         Router::mainRouter($url, $callback);
+}
 
 
 
-
-
-            }
+      }
 
         }
-    }
+
+
+
+
+}
+
+
+
 
 //put end
 
