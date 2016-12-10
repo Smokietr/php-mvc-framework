113d71370621143151c3c1c3c1f83faab2735ce2
Dılo abinin yeri <dilsizkaval@windowslive.com>
2016-12-10 18:44:48 +0200

    Add files via upload

---
+++ system/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kutuphane
+ * Date: 10.12.2016
+ * Time: 18:17
+ */
+class config {
+
+public function settings(){
+
+
+return array(
+      "host"=>"localhost",//database host
+      "dbname"=>"deneme", //database name
+      "user"=>"root", //database user
+      "password"=>"" //database password
+
+);
+   }
+}
