553126b543ac250a58ebfa63e89c3503821401eb
Dılo abinin yeri <dilsizkaval@windowslive.com>
2016-12-22 12:23:51 +0200

    Update ders.php

--- app/controllers/ders.php
+++ app/controllers/ders.php
@@ -21,10 +21,10 @@ $ses=(new session)->set(["ad"=>"dılo abinin yeri"]);
 
         /*
          (new users())->get(); hesini çeker
-        (new users())->where("id=2")->get();  id si 2 olan data
+        (new users())->where(["id"=>2])->get();  id si 2 olan data
         (new users())->find(2);    id 2 olan data otomatik id atanmıştır
         (new users())->delete(); tüm dataları siler
-        (new users())->where("id=2")->delete();  id 2olanı siler
+        (new users())->where(["id"=>2])->delete();  id 2olanı siler
          */
 
        $param2;  //urlde ki 2. parametreyi alır değişken ismi önemli değildir.url de kaç parametre varsa o kadar parametre alabilir bu fonskiyon
