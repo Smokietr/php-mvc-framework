7144f0819f9b9a1421aae9fe6f0b35136ab4b339
Dılo abinin yeri <dilsizkaval@windowslive.com>
2017-03-11 18:14:58 +0200

    Update autoload.php

--- system/autoload.php
+++ system/autoload.php
@@ -2,7 +2,7 @@
 
 
 
-include_once "vendor/autoload.php";
+//include_once "vendor/autoload.php";
 //set_include_path(implode(PATH_SEPARATOR, array(get_include_path(),'./controllers', './Models')));
 //set_include_path(implode(PATH_SEPARATOR, array('./controllers', './Models')));
 
@@ -12,6 +12,6 @@ include_once "vendor/autoload.php";
  */
 //
 
-/*set_include_path("app/controllers".PATH_SEPARATOR."app/Models".PATH_SEPARATOR."system/helpers/general".PATH_SEPARATOR."system/engine".PATH_SEPARATOR."system/cartclass/cart");
-spl_autoload_register();*/
+set_include_path("app/controllers".PATH_SEPARATOR."app/Models".PATH_SEPARATOR."system/helpers/general".PATH_SEPARATOR."system/engine".PATH_SEPARATOR."system/cartclass/cart");
+spl_autoload_register();
 
